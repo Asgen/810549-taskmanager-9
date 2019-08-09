7d2033a3090acabc58e60da849ee2a3c73e9c87f
Henry <genuus1@gmail.com>
2019-08-09 15:53:32 +0300

    t2-1

---
+++ src/components/load-button.js
@@ -0,0 +1,3 @@
+const getLoadButton = () => `<button class="load-more" type="button">load more</button>`;
+
+export {getLoadButton as button};
---
+++ src/components/search.js
@@ -0,0 +1,9 @@
+const getSearchTemplate = () => {
+  return `
+  <section class="main__search search container">
+    <input type="text" id="search__input" class="search__input" placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE">
+    <label class="visually-hidden" for="search__input">Search</label>
+   </section>`;
+};
+
+export {getSearchTemplate as search};
